5ee30ea9b724758eff7ce86e1e5db4e3df05b9bc
Matheus Carmo <me@matheuslc.com>
2014-11-11 01:53:21 -0200

    Function that populate the table, helper functions, ideb average function

--- js/main.js
+++ js/main.js
@@ -15,14 +15,19 @@
    *
    */
   APP.init = function() {
-    APP.AJAX('js/data/ideb.json', function(e) {
-      console.log(e);
+    APP.AJAX('js/data/ideb.json', function(json) {
+
+      var data = JSON.parse(json);
+
+      APP.populate(data);
+
     });
   };
 
 
   /* AJAX function wrapper
-   *
+   * @param url {string} Json URL
+   * @param callback (function) Callback function
    */
    APP.AJAX = function(url, callback) {
     var req = new XMLHttpRequest(),
@@ -37,6 +42,53 @@
 
     req.open("GET", url, true);
     req.send();
+
+   };
+
+
+   /* Change commas for dot
+   * @param before {string} String with comma value
+   */
+   APP.removeDot = function(before) {
+     return before.replace(/,/g, '.');
+   };
+
+   /* Change dor for comma
+   * @param before {string} String with dot value
+   */
+   APP.removeComma = function(before) {
+     return before.replace(/\./g, ',');
+   };
+
+   /* Average between the two given parameters
+   * @param flow {string} Flow value
+   * @param learn (string) Lean value
+   */
+   APP.calcIdeb = function(flow, learn) {
+     var Iflow  = APP.removeDot(flow),
+         Ilearn = APP.removeDot(learn);
+
+     return parseFloat(Iflow) * parseFloat(Ilearn);
+   }
+
+   /* AJAX function wrapper
+   * @param data {json} IDEB Data
+   * @param callback (function) Callback function
+   */
+   APP.populate = function(data) {
+     var i      = 0,
+         size   = data.city.length,
+         table  = document.querySelector('.ideb-tbody');
+
+     for(i; i < size; i++) {
+        var value = APP.calcIdeb(data.city[i].flow, data.city[i].learn);
+        var html =
+          "<tr>" +
+          "<td class='city-name'>" + data.city[i].name + "</td>" +
+          "<td class='flow-calc'>" + value + "</td>";
+     }
+
+     table.insertAdjacentHTML('beforeBegin', html);
 
    }
 
